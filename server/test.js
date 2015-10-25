3e3f2a0ab72c121440e9e7e37f27a8470fb7396d
Ronaldo Barbachano <redcap3000@gmail.com>
2015-10-24 17:14:44 -0800

    reducing data cache size

--- server/test.js
+++ server/test.js
@@ -11,7 +11,7 @@ Meteor.startup(function(){
 			console.log(new Date());
 			RunCli.run("redis-cli flushall");
 		},
-	1000*60*15);
+	1000*60*5);
 	Meteor.setInterval(function(){
 		var time =  Math.round(new Date() / 1000,2);
 		var ltcPrice = bitfinex_ticker('ltcusd','last_price');
